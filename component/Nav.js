10f2533bfed359d85a9336e0097dd6f6df19a139
earllee333 <jerrykiet2012@gmail.com>
2021-05-05 16:36:24 +0800

    v1.2

--- component/Nav.js
+++ component/Nav.js
@@ -8,17 +8,26 @@ export default function Navbar() {
     <div className="container">
       <nav>
           <h1>Kaohsiung</h1>
-          <ul>
+          <ul className="drop-down-menu">
+              <li><a>Functions</a>
+              <ul>
               <li><Link href="/" >Home</Link></li>
-              <li><Link href="/about" >About</Link></li>
-              <li><Link href="/contact" >Contact us</Link></li>
-              <li><Link href="/blogs" >Cars</Link></li>
-
+                <li><Link href="/about" >About</Link></li>
+                <li><Link href="/contact" >Contact us</Link></li>
+                <li><Link href="/blogs" >Cars</Link></li>
+              </ul>
+              </li>
+              
               {user&&<li><Link href="/recipes" >recipe</Link></li>}
               {!user &&<li className="btn" onClick={login} >Login</li>}
               {user&& <li className="btn" onClick={logout} >Louout</li>}
           </ul>
       </nav>
+      <style jsx>
+        {`{
+          .drop-down-menu ul{color:red}
+        }`}
+      </style>
     </div>
   )
 }
