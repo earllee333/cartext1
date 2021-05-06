cf7366cc4f12ee6c84a4f17f19a25716ca33e58a
earllee333 <jerrykiet2012@gmail.com>
2021-05-06 14:54:00 +0800

    vv1

--- component/Nav.js
+++ component/Nav.js
@@ -15,12 +15,15 @@ export default function Navbar() {
                 <li><Link href="/about" >About</Link></li>
                 <li><Link href="/contact" >Contact us</Link></li>
                 <li><Link href="/blogs" >Cars</Link></li>
+                <li>
+                  {user&&<li><Link href="/recipes" >recipe</Link></li>}
+              {!user &&<li className="btn" onClick={login} >Login</li>}
+              {user&& <li className="btn" onClick={logout} >Louout</li>}
+                </li>
               </ul>
               </li>
               
-              {user&&<li><Link href="/recipes" >recipe</Link></li>}
-              {!user &&<li className="btn" onClick={login} >Login</li>}
-              {user&& <li className="btn" onClick={logout} >Louout</li>}
+              
           </ul>
       </nav>
       <style jsx>
