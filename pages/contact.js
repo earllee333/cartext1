11034105bec09608ddcc5eca049844d2045e6ce7
earllee333 <jerrykiet2012@gmail.com>
2021-05-06 14:08:32 +0800

    vvvv

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-
+import Image from 'next/image'
 const Contact = () => {
     return ( 
     
@@ -7,23 +7,65 @@ const Contact = () => {
             <Head>
                 <title>Contact US!!</title>
             </Head>
-            <ul>
-                <li>Line:123@gmail.com</li>
-                <li>Ig:123@gmail.com</li>
-                <li>Facebook:123@gmail.com</li>
-                <li>Phone:123@gmail.com</li>
-                <li>Address:123@gmail.com</li>
+            <ul>      
+               <li>
+                   <ul>
+                   <li> <Image src='/line-icon.png' width={24} height={24}></Image>
+                </li>
+                <li>Line@12345678</li>
+                   </ul>
+               </li>
+               <li>
+                   <ul>
+                   <li> <Image src='/fb-icon.png' width={24} height={24}></Image>
+                </li>
+                <li>Fb@12345678</li>
+                   </ul>
+               </li>
+               <li>
+                   <ul>
+                   <li> <Image src='/ig-icon.jpg' width={24} height={24}></Image>
+                </li>
+                <li>Ig@12345678</li>
+                   </ul>
+               </li>
+               <li>
+                   <ul>
+                   <li> <Image src='/phone-icon.png' width={24} height={24}></Image>
+                </li>
+                <li>Phone@12345678</li>
+                   </ul>
+               </li>   
             </ul>
         <style jsx>
             {`{
+            ul li{
+                display:inline;
+                text-align:center;
+            }
             li{
                 text-align:center;
-                margin:10px;
-                font-size:20px;
+                margin:10px auto;
+                font-size:28px;
                 color:gray;
                 list-style-type: none;;
                 font-weight:600;
+                display:block;
+                padding:1px;
+                line-height:20px;
+                                              
                 
+            }
+
+            ul li ul li{display:flex
+                text-algin:center;
+                
+            }
+            ul  {
+                text-align:center;
+                padding:10px;
+                margin:10px ;
+                padding-top:30px               
             }
             }`}
         </style>
